33ef0b0347d9a3ebd0c7ea91d4abdea02d99bddc
Brandon Cullifer <brandon.cullifer@gmail.com>
2023-02-09 18:33:53 -0500

    get a list of choices

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
+// Get password length and then check that it meets length parameters.
     var getPasswordLength = prompt("Pick your Password length of at least 8 characters and no more than 128 characters.")
     if (getPasswordLength < 8){
         alert("Password must contain at least 8 characters!")
@@ -14,25 +15,46 @@ function writePassword() {
         alert("Password can't contain more than 128 characters!")
     }
     else
+// Ask user what characters they want to use.
     var getLowercase = confirm("Do you want to include lowercase? If so click ok.");
     var getUppercase = confirm("Do you want to include uppercase? If so click ok.");
     var getNumbers = confirm("Do you want to include numbers? If so click ok.");
     var getSpecialCharacters = confirm("Do you want to include special characters? If so click ok.");
     console.log(getPasswordLength, getLowercase, getUppercase, getNumbers, getSpecialCharacters);
+    if (!getLowercase && !getUppercase && !getNumbers && !getSpecialCharacters){
+        alert("You must pick SOMETHING!");
+    };
+   
+    // Create arrays to use for password characters
     var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     var specialCharacters = ['[`!@#$%^&*()_+-=[]{};\':"\\|,.<>/?~]/'];
+    
+    // Create password.  
     function generatePassword(){
-        var password = []
-        for (var i = 0; i < getPasswordLength; i++){
-            password.push(capitalLetters.charAt(Math.floor(Math.random() * getPasswordLength)))
+        var password = '';
+        var characters = [getLowercase, getUppercase, getNumbers, getSpecialCharacters];
+        var charactersChoice = [];
+        var characterPool;
+    // Made array for the characters that are asked for.
+        for (var i = 0; i < characters.length; i++ ){
+            if (characters[i]){
+                console.log(characters)
+                charactersChoice.push(characters[i]);
+            }
         }
-        return password
+
+        console.log(charactersChoice)
+        for (var i = 0; i < charactersChoice.length; i++){
+            characterPool = characterPool.concat(charactersChoice[i])
+        }
+        console.log(characterPool)
     }
     
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
+    console.log(password)
 
   passwordText.value = password;
 
